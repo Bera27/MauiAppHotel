7099500263509af1ab7c1f39f924658897b8cf32
Gabriel Bera <g.bera2710@gmail.com>
2025-03-07 20:08:53 -0300

    Projeto Completo

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiAppHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+        public int QuantidadeAdultos { get; set; }
+        public int QuantidadeCrianca { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+
+        public int Estadia
+        {
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+
+        public decimal ValorTotal
+        {
+            get
+            {
+                decimal valorAdultos = QuantidadeAdultos * QuartoSelecionado.ValorDiariaAdulto;
+                decimal valorCrianca = QuantidadeCrianca * QuartoSelecionado.ValorDiariaCrianca;
+
+                decimal total = (valorAdultos + valorCrianca) * Estadia;
+
+                return total;
+            }
+        }
+    }
+}
